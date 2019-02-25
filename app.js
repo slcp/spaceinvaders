193f503ed00a98041b07f6d679788c4051628035
iuketaylor <luketaylorrrr@hotmail.co.uk>
2019-02-25 16:41:52 +0000

    added comments for placeholders

--- app.js
+++ app.js
@@ -2,9 +2,11 @@ function draw() {
     let canvas = document.getElementById('game-canvas');
     let ctx = canvas.getContext('2d');
     
+    // bad ship recetangle placeholder
     ctx.fillStyle = 'white';
     ctx.fillRect(10, 10, 75, 50);
     
+    // good ship rectangle placeholder
     ctx.fillStyle = '#21c521';
     ctx.fillRect(350, 500, 100, 75);
 }
