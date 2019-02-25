af1c81022702e9c65b9acc04cc43f333ef1b3872
iuketaylor <luketaylorrrr@hotmail.co.uk>
2019-02-25 16:39:46 +0000

    created draw function

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
-let canvas = document.getElementById('game-canvas');
-let ctx = canvas.getContext('2d');
+function draw() {
+    let canvas = document.getElementById('game-canvas');
+    let ctx = canvas.getContext('2d');
+    
+    ctx.fillStyle = 'white';
+    ctx.fillRect(10, 10, 75, 50);
+    
+    ctx.fillStyle = '#21c521';
+    ctx.fillRect(350, 500, 100, 75);
+}
 
-ctx.fillStyle = 'white';
-ctx.fillRect(10, 10, 75, 50);
-
-
-ctx.fillStyle = '#21c521';
-ctx.fillRect(350, 500, 100, 75);
+draw();
