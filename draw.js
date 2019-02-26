73ff5018d8aef7c7b8874e79a0612e9bb0f4efe1
Stuart <stuartrforrest@gmail.com>
2019-02-26 16:21:51 +0000

    proof of concept drawing spave invader on canvas

--- draw.js
+++ draw.js
@@ -1,1 +1,42 @@
-canvas = document.getElementById('space-invaders');
+let canvas = document.getElementById('space-invaders');
+let context = canvas.getContext("2d");
+
+class TestShip {
+    constructor() {
+        this.shapes = [
+            {
+                x: 5,
+                y: 10,
+                width: 15,
+                height: 5
+            },
+            {
+                x: 10,
+                y: 5,
+                width: 5,
+                height: 5
+            },
+            {
+                x: 5,
+                y: 15,
+                width: 5,
+                height: 5
+            },
+            {
+                x: 15,
+                y: 15,
+                width: 5,
+                height: 5
+            }
+        ];
+    }
+    
+    draw() {
+        for (let shape of this.shapes) {
+            context.fillRect(shape.x, shape.y, shape.width, shape.height);
+        }
+    }
+}
+
+ship = new TestShip;
+ship.draw();
