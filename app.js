b6be017c0d6c46ea7f430c48917f457a76eb097b
moejamil29 <codenation.mj@gmail.com>
2019-02-27 16:33:19 +0000

    Small changes added to canvas-branch nothing new from the last push

--- app.js
+++ app.js
@@ -11,4 +11,32 @@ function draw() {
     ctx.fillRect(350, 500, 100, 75);
 }
 
+let game = new SpaceInvadersGame('game-canvas');
+game.newGame();
+setInterval(() => {
+    game.moveBadShips();
+}, 100);
+
+const key_code_left = 37;
+const key_code_right = 39;
+const key_code_space = 32;
+
+function onkeydown(e){
+    console.log(e.keyCode === key_code_left)
+    if(e.keyCode === key_code_left){
+        console.log("left")
+        game_state.playerX -= 5;
+        const $player = document.querySelector(".player")
+        console.log($player)
+        setPosition($player, game_state.playerX, game_state.playerY);
+    } else if (e.keyCode === key_code_right){
+        console.log("right")
+        game_state.playerX += 5;
+        const $player = document.querySelector(".player");
+        console.log($player)
+        setPosition($player, game_state.playerX, game_state.playerY);
+    }
+}
+
 draw();
+window.addEventListener("keydown", onkeydown);
