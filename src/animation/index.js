6679c581748694c45c2fd74ed3f5dbe73c361ce4
Stuart Forrest <stuartrforrest@gmail.com>
2021-08-07 22:03:50 +0100

    simplify

--- src/animation/index.js
+++ src/animation/index.js
@@ -11,11 +11,7 @@ export const runFrame = (frames, frameStartTime = 0) => {
   // this action should run. If yes, run the action.
   frames.forEach(({ id, action, ms }) => {
     // If there is no last run time set it and run, it must be the beginning of the game
-    if (!lastActionFrame[id]) {
-      lastActionFrame[id] = frameStartTime;
-      return action();
-    }
-    if (frameStartTime - lastActionFrame[id] > ms) {
+    if (!lastActionFrame[id] || (frameStartTime - lastActionFrame[id] > ms)) {
       lastActionFrame[id] = frameStartTime;
       return action();
     }
