b724b28e92011659d425f57c0244e33a063d6492
Stuart Forrest <stuart.forrest@infinityworks.com>
2021-08-22 20:39:20 +0100

    remove unused code

--- src/player/player.js
+++ src/player/player.js
@@ -8,68 +8,6 @@ import {
   SET_SCORE,
 } from "../events/events";
 
-// class Player {
-//   constructor(context) {
-//     this.id = Symbol();
-//     this.context = context;
-//     this._score = 0;
-//     this.lives = 3;
-//   }
-
-//   init() {
-//     const { eventBus } = this.context;
-//     eventBus.subscribe(
-//       GOOD_SHIP_KILLED_BY_BAD_BULLET,
-//       this.playerKilled.bind(this)
-//     );
-//     eventBus.subscribe(
-//       BAD_SHIP_KILLED_BY_GOOD_BULLET,
-//       this.badShipSkilled.bind(this)
-//     );
-//   }
-
-//   playerKilled({ id }) {
-//     const { eventBus } = this.context;
-//     if (id !== this.id) return;
-//     this.lives = this.lives - 1;
-//     eventBus.publish(PLAYER_LOST_LIFE, {
-//       id: this.id,
-//       remainingLives: this.lives,
-//     });
-//     eventBus.publish(LOSE_LIFE, { id });
-//     if (this.lives) {
-//       eventBus.publish(RESPAWN_GOOD_SHIP, { id: this.id });
-//     }
-//   }
-
-//   badShipSkilled({ id }) {
-//     if (id !== this.id) return;
-//     this.score = this.score + 10;
-//   }
-
-//   set score(value) {
-//     this._score = value;
-//     const { eventBus } = this.context;
-//     eventBus.publish(SET_SCORE, { id: this.id, score: this.score });
-//   }
-
-//   get score() {
-//     return this._score;
-//   }
-// }
-
-/*
- * Plan:
- * Create a player for each player - Done
- * Inject player ids into the game - Done
- * Game maps the player ids and assigns them to goodShips - Done
- * Score(Managers?) are also created by mapping the plaer ids (outside of the gmae)
- * Events that concern a goodShip pass the player id as an arg - Done
- * Both Score(Managers?) and Player subscribe to events to maintain internal state/update the UI - Players Done
- * Result:
- * Score UI/Player data/Game run are decoupled but can communicate about players
- */
-
 export const PLAYER_TYPE = "_player";
 
 let players = [];
@@ -89,9 +27,7 @@ const handlePlayerKilled = (bus) => ({ id }) => {
 const handleBadShipKilled = (bus) => ({ id }) => {
   const player = players.find((p) => id === p.id);
   if (!player) {
-    throw new Error(
-      `player with id '${id}' is not known, cannot set score`
-    );
+    throw new Error(`player with id '${id}' is not known, cannot set score`);
   }
   player.score = player.score + 10;
   publishToEventBus(bus, SET_SCORE, { id: player.id, value: player.score });
