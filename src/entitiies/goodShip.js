508ab5e4513e76a337aede2d964962874b418801
Stuart Forrest <stuart.forrest@infinityworks.com>
2021-08-23 11:40:10 +0100

    remove unused code

--- src/entitiies/goodShip.js
+++ src/entitiies/goodShip.js
@@ -1,15 +1,14 @@
 import { v4 as uuid } from "uuid";
 import { runFrame } from "../animation";
 import {
-  initialiseAnimationFrame,
-  newAnimationFrame,
+  newAnimationFrame
 } from "../animation/animationFrame";
 import { newShape } from "../canvas/shape";
 import { publishToEventBus, subscribeToEventBus } from "../events";
 import {
   BULLET_CREATED,
   BULLET_DESTROYED,
-  CANVAS_DRAW,
+  CANVAS_DRAW
 } from "../events/events";
 import moveObject from "../functional/moveObject";
 import { ARROW_LEFT, ARROW_RIGHT, SPACE } from "../keyCodes";
@@ -17,28 +16,6 @@ import { fireBullet } from "./bullet";
 import { newShip } from "./ship";
 
 // class GoodShip extends Ship {
-//   constructor({ game, settings, eventBus, id }) {
-//     super(game, settings, eventBus);
-//     this.id = id;
-//     this.keys = [];
-//     this.shapes = [
-//       new Shape(20, 40, 60, 20, "#21c521"),
-//       new Shape(40, 20, 20, 20, "#21c521"),
-//       new Shape(20, 55, 20, 20, "#21c521"),
-//       new Shape(60, 55, 20, 20, "#21c521"),
-//     ];
-//     this.lives = 3;
-//     this.shootTrigger = SPACE;
-//     this.handleKeyDown = this.handleKeyDown.bind(this);
-//     this.handleKeyUp = this.handleKeyUp.bind(this);
-//     this.frameActions = [
-//       new AnimationFrame(Symbol("moveGoodShip"), 0, () => this.moveShip()),
-//       new AnimationFrame(Symbol("fireGoodShipBullet"), 800, () =>
-//         this.fireBullet()
-//       ),
-//     ];
-//   }
-
 //   init() {
 //     this.eventBus.subscribe(
 //       GOOD_SHIP_KILLED_BY_BAD_BULLET,
@@ -62,71 +39,6 @@ import { newShip } from "./ship";
 
 //   isDead() {
 //     return this.lives < 0;
-//   }
-
-//   addEventListeners() {
-//     this.startAnimation();
-//     window.addEventListener("keydown", this.handleKeyDown);
-//     window.addEventListener("keyup", this.handleKeyUp);
-//   }
-
-//   startAnimation() {
-//     this.animation = new GameAnimation();
-//     this.animation.runFrame(this.frameActions);
-//   }
-
-//   moveShip() {
-//     if (this.keys["RIGHT"]) {
-//       moveObject({ object: this, deltaX: 2, deltaY: 0 });
-//       drawObject({ eventBus: this.eventBus, object: this });
-//       return;
-//     }
-//     if (this.keys["LEFT"]) {
-//       moveObject({ object: this, deltaX: -2, deltaY: 0 });
-//       drawObject({ eventBus: this.eventBus, object: this });
-//       return;
-//     }
-//   }
-
-//   handleKeyDown(event) {
-//     event.preventDefault();
-//     if (event.code === this.shootTrigger) {
-//       const immediateFire = !this.keys["SPACEBAR"];
-//       this.keys["SPACEBAR"] = true;
-//       if (immediateFire) {
-//         this.fireBullet();
-//       }
-//       return;
-//     }
-//     if (event.code === ARROW_LEfT && !this.direction) {
-//       this.keys["LEFT"] = true;
-//       return;
-//     }
-//     if (event.code === ARROW_RIGHT && !this.direction) {
-//       this.keys["RIGHT"] = true;
-//       return;
-//     }
-//   }
-
-//   () {
-//     if (!this.keys["SPACEBAR"]) return;
-//     super.fireBullet();
-//   }
-
-//   handleKeyUp(event) {
-//     event.preventDefault();
-//     if (event.code === this.shootTrigger) {
-//       this.keys["SPACEBAR"] = false;
-//       return;
-//     }
-//     if (event.code === ARROW_LEfT) {
-//       this.keys["LEFT"] = false;
-//       return;
-//     }
-//     if (event.code === ARROW_RIGHT) {
-//       this.keys["RIGHT"] = false;
-//       return;
-//     }
 //   }
 // }
 
